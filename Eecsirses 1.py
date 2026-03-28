355a9a506fc6ce326c294609e2667b0f189374c8
noor10-sh <nooralhadashaheen@gmail.com>
2026-03-28 22:01:12 +0300

    Add files via upload

---
+++ Eecsirses 1.py
@@ -0,0 +1,37 @@
+#Excersise1:
+#Q1:
+penne_price=16.68
+arrabiata_pasta_sauce=6.98
+bag_organic=16.78
+italian_seasoning=15.26
+artisan_baguettes=3
+bag_meatballs=4.39
+subtotal=penne_price+arrabiata_pasta_sauce+bag_organic+italian_seasoning+artisan_baguettes+bag_meatballs
+print(f'The subtotal of items ={round(subtotal,2)}')
+print('************************************************')
+
+#Q2:
+num=2.3
+print(type(num))
+print(str(num)+' is a float')
+print("Hello,l'm Noor , it\'s nice to meet you!")
+print('************************************************')
+
+#Q3:
+n=7
+print('*'*n+'\n'+'\b *'*(n-2)+'\n' +' *'*(n-2)+'\n'+'   *'*(n-2))
+print('************************************************')
+#Q4:
+name = input('What is your name?\n')
+quest= input('What is your quest?\n')
+color= input('What is your favorite color?\n')
+print(f'So your name is {name} , your quest is {quest} , and your favorite color is {color}')
+print('************************************************')
+#Q5:
+n1 = int(input('Enter first number:'))
+n2 = int(input('Enter second number:'))
+n3 = int(input('Enter third number:'))
+print(f'The average of these number={(n1+n2+n3)/3}')
+print('************************************************')
+
+
